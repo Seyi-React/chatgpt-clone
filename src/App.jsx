76fd6ddb8a80b4f9b128b50145d44bec981375b7
Seyi-React <oluwaseyiadeoti825@gmail.com>
2023-11-26 21:53:39 +0100

    main-in-progress

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import chat from "./assets/download.png";
 import { FaPlus } from "react-icons/fa";
 import { BsChatRight } from "react-icons/bs";
+import { BiSolidHome } from "react-icons/bi";
+import { FaTag } from "react-icons/fa";
+import { IoRocketSharp } from "react-icons/io5";
+import { IoSendSharp } from "react-icons/io5";
+import baby from "./assets/baby.webp";
+import doctor from "./assets/doctor.jpeg";
 
 const App = () => {
   return (
@@ -28,12 +34,8 @@ const App = () => {
                   }}
                 >
                   {" "}
-                  <FaPlus
-                    color="white"
-                   
-                    style={{ fontWeight: "bold" }}
-                  />{" "}
-                  New Chat
+                  <FaPlus color="white" style={{ fontWeight: "bold" }} /> New
+                  Chat
                 </button>
                 {/* what is ???? */}
                 <div className="what-is">
@@ -58,7 +60,7 @@ const App = () => {
                     }}
                   >
                     {" "}
-                    <BsChatRight  />
+                    <BsChatRight />
                     How to Use API?
                   </button>
                 </div>
@@ -66,10 +68,86 @@ const App = () => {
             </div>
           </div>
           {/* lower */}
-          <div className="lower"></div>
+          <div className="lower">
+            <div
+              className="list"
+              style={{
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                gap: "10px",
+              }}
+            >
+              <BiSolidHome />
+              Home
+            </div>
+            <div
+              className="list"
+              style={{
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                gap: "10px",
+              }}
+            >
+              <FaTag />
+              Save
+            </div>
+            <div
+              className="list"
+              style={{
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                gap: "10px",
+              }}
+            >
+              <IoRocketSharp />
+              Upgrade to Pro
+            </div>
+          </div>
         </div>
         {/* main */}
-        <div className="main"></div>
+        <div className="main">
+          <div className="chats">
+            <div className="chat">
+              <img
+                src={baby}
+                alt=""
+                width={40}
+                height={40}
+                style={{ borderRadius: "50%" }}
+              />
+              <p className="text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                quaerat saepe repudiandae, doloribus possimus deleniti fuga
+                similique beatae amet, asperiores laborum explicabo
+                exercitationem repellat eos.
+              </p>
+            </div>
+            <div className="chat">
+              <img
+                src={doctor}
+                alt=""
+                width={40}
+                height={40}
+                style={{ borderRadius: "50%" }}
+              />
+              <p className="text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                quaerat saepe repudiandae, doloribus possimus deleniti fuga
+                similique beatae amet, asperiores laborum explicabo
+                exercitationem repellat eos.
+              </p>
+            </div>
+          </div>
+          <div className="chat-footer">
+            <div className="input">
+              <input type="text" name="" id="" />
+              <IoSendSharp />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
